793f9db0ed8152ef93b0cdefc6a7ad9a4148d243
Epoisse <anton.anokhin.qa@gmail.com>
2022-06-07 14:53:26 -0400

    DashboardPage has been added

--- src/test/java/utils/PageInitializers.java
+++ src/test/java/utils/PageInitializers.java
@@ -1,13 +1,16 @@
 package utils;
 
+import pages.DashboardPage;
 import pages.LoginPage;
 
 public class PageInitializers {
 
     public static LoginPage loginPage;
+    public static DashboardPage dashboardPage;
 
     public static void initializePageObjects() {
         // initializing objects of the LoginPage class
         loginPage = new LoginPage();
+        dashboardPage = new DashboardPage();
     }
 }
